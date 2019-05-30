58621708716c3fdf5bca07bca98770359ae92272
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2019-05-30 19:01:32 -0300

    strategy

---
+++ src/strategy.kt
@@ -0,0 +1,23 @@
+interface ConvertStrategy{
+	fun convert(map: Map<String, String>);
+}
+
+class ConvertToJson: ConvertStrategy{
+	override fun convert(map: Map<String, String>){
+		var json: String = "{\n";
+		for((k,v) in map){
+			json+="\t\"$k\":\"$v\",\n";
+		} 
+		json+="}";
+		println(json);
+	}
+}
+
+//class ConvertToXML: ConvertStrategy{
+//	override fun convert(map: Map)
+//}
+
+fun main(){
+	var paraJson = ConvertToJson();
+	paraJson.convert(mapOf("Foi" to "Embora","Vamos" to "pra casa"));
+}
