26047cba904cf6f8e5de1b5e9717459fdfe09d7c
MailsonD <mailssondennis@gmail.com>
2019-05-31 15:43:49 -0300

    Implementando exemplo para o padrão State

--- src/State/Exemplo1.kt
+++ src/State/Exemplo1.kt
@@ -1,2 +1,58 @@
 package State
 
+interface EstadoDeAlerta{
+    fun alertar()
+}
+
+class Vibracao : EstadoDeAlerta{
+
+    override fun alertar() {
+        println("VRUMMMMM!")
+    }
+
+}
+
+class Som : EstadoDeAlerta{
+
+    override fun alertar() {
+        println("Piririm Piririm Piririm!")
+    }
+
+}
+
+class Mudo : EstadoDeAlerta {
+
+    override fun alertar() {
+        println("........")
+    }
+
+}
+
+
+class Alerta{
+
+    private var estadoDeAlerta: EstadoDeAlerta = Vibracao()
+
+    fun setEstado(estado: EstadoDeAlerta){
+        estadoDeAlerta = estado
+    }
+
+
+    fun alertar(){
+        estadoDeAlerta.alertar()
+    }
+
+}
+
+fun main(args: Array<String>) {
+
+    val meuAlerta = Alerta()
+    meuAlerta.alertar()
+    meuAlerta.setEstado(Som())
+    meuAlerta.alertar()
+    meuAlerta.setEstado(Mudo())
+    meuAlerta.alertar()
+
+}
+
+
