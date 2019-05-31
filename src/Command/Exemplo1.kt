06c303cb78e37b0362008a9577c1b1884f07ea5a
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2019-05-31 14:57:06 -0300

    Padrão command completo

--- src/Command/Exemplo1.kt
+++ src/Command/Exemplo1.kt
@@ -5,20 +5,32 @@ interface Command{
 }
 
 class MensagemPorSMSCommand: Command {
-	override fun execute(msg: String): Unit{
+	override fun execute(msg: String){
 		println("Mensagem eviada aos funcionários por SMS: $msg");
 	}
 }
 
 class MensagemPorEmailCommand: Command {
-	override fun execute(msg: String): Unit{
+	override fun execute(msg: String){
 		println("Mensagem eviada aos funcionários por email: $msg");
 	}
 }
 
+class EnviarMensagensFuncionarios(){
+	private val queue = ArrayList<Command>();
+	
+	fun addCommand(novoCommand: Command){
+		queue.add(novoCommand);
+	}
+	
+	fun enviarMensagem(msg: String){
+		queue.forEach {it.execute(msg)}
+	}
+}
+
 fun main(){
-	var msgEmail : Command = MensagemPorEmailCommand();
-	var msgSMS: Command = MensagemPorSMSCommand();
-	msgEmail.execute("Reunião as 15h");
-	msgSMS.execute("Reunião as 15h");
+	var commandProcessor: EnviarMensagensFuncionarios = EnviarMensagensFuncionarios();
+	commandProcessor.addCommand(MensagemPorEmailCommand());
+	commandProcessor.addCommand(MensagemPorSMSCommand());
+	commandProcessor.enviarMensagem("Reunião as 16h");
 }
