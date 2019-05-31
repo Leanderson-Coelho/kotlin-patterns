541b4c507f16faa68dd1b5666a6a2d988c32ff04
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2019-05-31 14:28:46 -0300

    Subindo padrão com erro

--- src/Command/Exemplo1.kt
+++ src/Command/Exemplo1.kt
@@ -1,2 +1,24 @@
 package Command
 
+interface Command{
+	fun execute(msg: String): Unit;
+}
+
+class MensagemPorSMSCommand: Command {
+	override fun execute(msg: String): Unit{
+		println("Mensagem eviada aos funcionários por SMS: $msg");
+	}
+}
+
+class MensagemPorEmailCommand: Command {
+	override fun execute(msg: String): Unit{
+		println("Mensagem eviada aos funcionários por email: $msg");
+	}
+}
+
+fun main(){
+	var msgEmail : Command = MensagemPorEmailCommand();
+	var msgSMS: Command = MensagemPorSMSCommand();
+	msgEmail.execute("Reunião as 15h");
+	msgSMS.execute("Reunião as 15h");
+}
