2c313dd6f7f684ad332ef302661bc47d3f9cfcbb
MailsonD <mailssondennis@gmail.com>
2019-06-09 15:54:26 -0300

    Refinando as classes para o padrão observer

--- src/Observer/Exemplo1.kt
+++ src/Observer/Exemplo1.kt
@@ -10,33 +10,45 @@ class Pessoa(val nome: String)
 
 class NotificaPessoa(private val pessoa: Pessoa): Notificacao{
     override fun notificar(mensagem: String) {
-        println("${pessoa.nome} recebeu a mensagem: \"$mensagem\" com sucesso!")
+        println("${pessoa.nome} recebeu a mensagem: \"mensagem\" com sucesso!")
     }
 }
 
-class Chat(private val notificadores : List<NotificaPessoa>, private val mensagens: ArrayList<String> = ArrayList()){
+class Chat(private val notificadores : ArrayList<Notificacao> = ArrayList()){
 
-    private var mensagem: String by Delegates.observable("") { _, _, new ->
+    private val mensagens: ArrayList<String> = ArrayList()
+
+    private var ultimaMensagem: String by Delegates.observable(""){ _, _, new ->
         mensagens.add(new)
-        for(not in notificadores){
-            not.notificar(new)
-        }
+        notificadores.forEach { notf -> notf.notificar(new) }
     }
 
-    fun add(mensagemNova: String){
-        mensagem = mensagemNova
+
+    fun enviar(mensagemNova: String){
+        ultimaMensagem = mensagemNova
+    }
+
+    fun addNotificador(notificador : Notificacao){
+        notificadores.add(notificador)
+    }
+
+    fun removerNotificador(notificador: Notificacao){
+        notificadores.remove(notificador)
     }
 
 }
 
 fun main(args: Array<String>) {
 
-    var not1 = NotificaPessoa(Pessoa("eu"))
-    var not2 = NotificaPessoa(Pessoa("tu"))
+    val not1 = NotificaPessoa(Pessoa("eu"))
+    val not2 = NotificaPessoa(Pessoa("tu"))
 
-    var chat = Chat(listOf(not1,not2))
-    chat.add("teste")
-    chat.add("teste2")
+    val chat = Chat(arrayListOf(not1,not2))
+
+    chat.addNotificador(NotificaPessoa(Pessoa("ele")))
+    chat.enviar("opa")
+    chat.enviar("Mah Oe")
+
 
 
 }
